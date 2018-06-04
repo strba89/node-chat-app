bb3f0b1bffc51ff64222d3ee6d81a1c67e68ff98
strbo <strbo89@yahoo.com>
2018-06-04 16:13:48 +0200

    Add connect and disconnect events whit socket.io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
+
+
+
+
 const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname,'../public');
+
 
 let app = express();
+let server = http.createServer(app);
 
+let io = socketIO(server);
 
-const publicPath = path.join(__dirname,'../public');
+io.on('connection', (socket)=>{
+    console.log('New user connect');
+    socket.on('disconnect', ()=> {
+        console.log('User was disconnect')
+    });
+});
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`Started on port ${port}`)
 });
 
